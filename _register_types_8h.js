279ffd5babfcd5924078a4558e0e69a723b9b0a3
jrouwe <jrouwe@users.noreply.github.com>
2023-07-05 18:11:36 +0000

    deploy: b6c184a25d8f32527c0a217ac5c512f61f26f21a

--- _register_types_8h.js
+++ _register_types_8h.js
@@ -1,5 +1,6 @@
 var _register_types_8h =
 [
-    [ "RegisterTypes", "_register_types_8h.html#a5f763c6acab87b2d5e31b9fe245e8522", null ],
+    [ "RegisterTypes", "_register_types_8h.html#a68b7898e4d6e6b888cc10b364d6fe561", null ],
+    [ "RegisterTypesInternal", "_register_types_8h.html#a316f2a96a971b327ae6d9583455d7558", null ],
     [ "UnregisterTypes", "_register_types_8h.html#a1e0db6031789e773039c7fc15ef47057", null ]
 ];
